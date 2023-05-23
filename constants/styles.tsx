6f250be46951006b911be214d3f2d03c887fa1a5
Laps-F <maurolaps@gmail.com>
2023-05-23 20:57:11 -0300

    style: Creating constant for the NewColors of the Challenge

--- constants/styles.tsx
+++ constants/styles.tsx
@@ -5,3 +5,13 @@ export const Colors = {
     input: "#656262",
     inputPlaceholder: "#A8A8A8",
 }
+
+export const NewColors = {
+    buttonBuy_Add: "#20BE4C",
+    primary: "#D78F3C",
+    warning: "#FF4B4B",
+    background: "#282729",
+    cardProduct: "#3C3C3E",
+    input: "#656262",
+    inputPlaceholder: "#A8A8A8",
+}
