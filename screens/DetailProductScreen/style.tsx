6ddc0446b953a65a2b03767222ed8418366d26bd
Laps-F <maurolaps@gmail.com>
2023-05-23 23:45:02 -0300

    style: Adding the style for the card skeleton

--- screens/DetailProductScreen/style.tsx
+++ screens/DetailProductScreen/style.tsx
@@ -9,7 +9,7 @@ export const styles = StyleSheet.create({
         backgroundColor: NewColors.background,
     },
 
-    card: {
+    cardContainer: {
         flex: 1,
         margin: 35,
         width: "80%",
@@ -28,10 +28,51 @@ export const styles = StyleSheet.create({
         alignItems: 'center',
     },
 
-    button: {
+    buttonAdd: {
         flex: 1,
         width: "50%",
         borderRadius: 10,        
         backgroundColor: NewColors.buttonBuy_Add,
+    },
+
+    buttonPrice: {
+        width: "45%",
+        height: "14%",
+        borderRadius: 10,     
+        backgroundColor: NewColors.background,
+        justifyContent: "flex-start",
+        marginHorizontal: 0,
+        marginLeft: 'auto'
+    },
+
+    card: {
+        flex: 1,
+        marginVertical: 20,
+        marginHorizontal: 15,
+    },
+
+    title: {
+        color: "white",
+    },
+
+    imageContainer: {
+        flex: 1,
+        alignItems: 'center',
+    },
+
+    image: {
+        margin: 15,
+        width: "100%",
+        height: "40%",
+    },
+
+    rating: {
+        alignSelf: 'flex-start',
+        color: 'white',
+    },
+
+    priceAndCount: {
+        flex: 1,
+        flexDirection: 'row',
     }
 });
