41b0fb509ae6b9d907e5b07b3d1a4076574438af
Arthur Alexandre Cunha Custodio <arthur.custodio@aluno.ufop.edu.br>
2023-05-24 08:37:09 -0300

    Adding and styling the totalValue Container

--- screens/CartScreen/index.tsx
+++ screens/CartScreen/index.tsx
@@ -6,15 +6,19 @@ import { styles } from "./style";
 
 function ShoppingCartScreen() {
 
-    function Test(){
+    function Test() {
 
     }
 
     return (
         <View style={styles.root}>
             <View>
+                <View style={styles.totalPriceContainer}>
+                    <Text style={styles.totalText}>TOTAL</Text>
+                    <Text style={styles.totalValue}>R$ 00,00</Text>
+                </View>
                 <View style={styles.buttonContainer}>
-                    <MyButton title="BUY" onPress={Test} style={styles.button} color={NewColors.buttonBuy_Add}/>
+                    <MyButton title="BUY" onPress={Test} style={styles.button} color={NewColors.buttonBuy_Add} />
                 </View>
             </View>
         </View>
--- screens/CartScreen/style.tsx
+++ screens/CartScreen/style.tsx
@@ -2,11 +2,27 @@ import { StyleSheet, Dimensions } from "react-native";
 import { NewColors } from "../../constants/styles";
 
 const HEIGHT = Dimensions.get('window').height;
+const WIDTH = Dimensions.get('window').width;
 
 export const styles = StyleSheet.create({
     root: {
         flex: 1,
         backgroundColor: NewColors.background,
+    },
+    totalPriceContainer: {
+        height: 60,
+        alignItems: "flex-start",
+        left: "10%",
+        top: "20%",
+        //backgroundColor: NewColors.primary,
+    },
+    totalText: {
+        color: "white",
+        fontSize: 20,
+    },
+    totalValue: {
+        color: NewColors.primary,
+        fontSize: 32,
     },
     buttonContainer: {
         top: HEIGHT - 200,
@@ -18,7 +34,7 @@ export const styles = StyleSheet.create({
     button: {
         flex: 1,
         width: "50%",
-        borderRadius: 10,        
+        borderRadius: 10,
         backgroundColor: NewColors.buttonBuy_Add,
     },
 });
