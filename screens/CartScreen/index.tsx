64714074d436041899e9bb02c6eb49a21880d900
Arthur Alexandre Cunha Custodio <arthur.custodio@aluno.ufop.edu.br>
2023-05-24 08:20:43 -0300

    Using the new styled MyButton component

--- screens/CartScreen/index.tsx
+++ screens/CartScreen/index.tsx
@@ -1,5 +1,6 @@
 import { Text, View } from "react-native";
 import MyButton from "../../components/MyButton";
+import { NewColors } from "../../constants/styles";
 
 import { styles } from "./style";
 
@@ -12,8 +13,8 @@ function ShoppingCartScreen() {
     return (
         <View style={styles.root}>
             <View>
-                <View>
-                    <MyButton title="BUY" onPress={Test}/>
+                <View style={styles.buttonContainer}>
+                    <MyButton title="BUY" onPress={Test} style={styles.button} color={NewColors.buttonBuy_Add}/>
                 </View>
             </View>
         </View>
--- screens/CartScreen/style.tsx
+++ screens/CartScreen/style.tsx
@@ -1,9 +1,24 @@
 import { StyleSheet, Dimensions } from "react-native";
 import { NewColors } from "../../constants/styles";
 
+const HEIGHT = Dimensions.get('window').height;
+
 export const styles = StyleSheet.create({
     root: {
         flex: 1,
         backgroundColor: NewColors.background,
+    },
+    buttonContainer: {
+        top: HEIGHT - 200,
+        width: "100%",
+        position: 'absolute',
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    button: {
+        flex: 1,
+        width: "50%",
+        borderRadius: 10,        
+        backgroundColor: NewColors.buttonBuy_Add,
     },
 });
