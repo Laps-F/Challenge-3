15ffff0ef1b0935ffb82a86804b9f8c0ac4457fb
Laps-F <maurolaps@gmail.com>
2023-05-24 18:21:46 -0300

    style: Styling the Plus and Minus Button

--- components/PlusButton/index.tsx
+++ components/PlusButton/index.tsx
@@ -1,11 +1,11 @@
 import { Text, TouchableOpacity, View } from "react-native";
 import { styles } from "./style";
 
-function PlusButton() {
+function PlusButton({icon} : any): JSX.Element {
     return (
-        <TouchableOpacity>
-            <View style={styles.root}>
-                <Text style={styles.text}>+</Text>
+        <TouchableOpacity style={styles.root}>
+            <View style={styles.button}>
+                <Text style={styles.text}>{icon}</Text>
             </View>
         </TouchableOpacity>
         
