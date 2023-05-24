c30f44f79df7ac36ca766ff4ca3389ce4dcc0e19
Laps-F <maurolaps@gmail.com>
2023-05-23 23:44:29 -0300

    feat: Adding the first part of the card skeleton

--- screens/DetailProductScreen/index.tsx
+++ screens/DetailProductScreen/index.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { Text, View, ActivityIndicator } from "react-native";
+import { Text, View, Image } from "react-native";
 import MyButton from "../../components/MyButton";
 import { NewColors } from "../../constants/styles";
 
@@ -16,15 +16,44 @@ function DetailProductScreen() {
         }, 3000);
     }
 
+    function test(){
+
+    }
+
     return (
         <View style={styles.root}>
-            <View style={styles.card}>
+            <View style={styles.cardContainer}>
+                <View style={styles.card}>
+                    <Text style={styles.title}>Title of the product</Text>
+                    <View style={styles.imageContainer}>
+                        <Image source={require('../../assets/icon.png')} style={styles.image}/>
+                        <Text style={styles.rating}>Rating</Text>
+                        <View style={styles.priceAndCount}>
+                            <MyButton 
+                                title="R$ 300,00" 
+                                onPress={test}
+                                style={styles.buttonPrice} 
+                                color={NewColors.background}
+                            />
+                            <MyButton 
+                                title="R$ 300,00" 
+                                onPress={test}
+                                style={styles.buttonPrice} 
+                                color={NewColors.background}
+                            />
+
+                        </View>
+                        
+                    </View>
+                </View>
+
+
 
                 <View style={styles.buttonContainer}>
                     <MyButton 
                         title="ADD TO CART" 
                         onPress={addToCartHandler} 
-                        style={styles.button} 
+                        style={styles.buttonAdd} 
                         color={NewColors.buttonBuy_Add}
                         load={isLoading} 
                     />
