09cc7e5e75098d73a70177e48de44149fb9d654d
Arthur Alexandre Cunha Custodio <arthur.custodio@aluno.ufop.edu.br>
2023-05-26 03:14:53 -0300

    Adding the definition of AddToCartAction interface

--- redux/actions.tsx
+++ redux/actions.tsx
@@ -1,2 +1,8 @@
 import { Action } from 'redux';
 import ProductInterface from '../types/ProductInterface';
+
+export interface AddToCartAction extends Action {
+    type: 'ADD_TO_CART';
+    payload: ProductInterface;
+}
+  
