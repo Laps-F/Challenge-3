b0f7762bd35bf4a70f8738bca407219cae49b83d
Arthur Alexandre Cunha Custodio <arthur.custodio@aluno.ufop.edu.br>
2023-05-26 03:31:30 -0300

    Implementing the ShoppingCart Reductor

--- redux/reducers.tsx
+++ redux/reducers.tsx
@@ -8,3 +8,17 @@ export interface CartState {
 const initialState: CartState = {
     cartItems: [],
 };
+
+const cartReducer: Reducer<CartState> = (state = initialState, action) => {
+    switch (action.type) {
+        case 'ADD_TO_CART':
+            return {
+                ...state,
+                cartItems: [...state.cartItems, action.payload],
+            };
+        default:
+            return state;
+    }
+};
+
+export default cartReducer;
