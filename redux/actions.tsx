975bbe5b1a7806dfa12d55d91eac8bd6e24a965f
Arthur Alexandre Cunha Custodio <arthur.custodio@aluno.ufop.edu.br>
2023-05-26 03:18:11 -0300

    Adding function addToCart

--- redux/actions.tsx
+++ redux/actions.tsx
@@ -5,4 +5,10 @@ export interface AddToCartAction extends Action {
     type: 'ADD_TO_CART';
     payload: ProductInterface;
 }
-  
+
+export const addToCart = (product: ProductInterface): AddToCartAction => {
+    return {
+        type: 'ADD_TO_CART',
+        payload: product,
+    };
+};
