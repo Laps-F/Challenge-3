c7f793d9d9530929d122b3b7335a228481119160
lilisev <eliane.sevegnago@fatec.sp.gov.br>
2023-05-25 21:07:39 -0300

    feat:Sending the product object from HomeScreen to DetailProductScreen and implementing the function that receives the object.

--- screens/DetailProductScreen/index.tsx
+++ screens/DetailProductScreen/index.tsx
@@ -1,9 +1,59 @@
 import { Text, View } from "react-native";
 import MyButton from "../../components/MyButton";
 import { NewColors } from "../../constants/styles";
+import { Route } from "@react-navigation/native";
+
 
 import { styles } from "./style";
+import ProductInterface from "../../types/ProductInterface";
 
+
+type DetailProductScreenProps = {
+    route: {
+        params: {
+            product: ProductInterface & { description: string};
+        };
+    };
+};
+
+function DetailProductScreen({ route }: DetailProductScreenProps) {
+    const { product } = route.params;
+
+    function Test(){
+
+    }
+
+    return (
+        <View style={styles.root}>
+          <View style={styles.card}>
+            <Text>{product.title}</Text>
+            <Text>{product.image}</Text>
+            <Text>{product.price}</Text>  
+            <Text>{product.description}</Text>   
+            <View style={styles.buttonContainer}>
+              <MyButton
+                title="ADD TO CART"
+                onPress={Test}
+                style={styles.button}
+                color={NewColors.buttonBuy_Add}
+              />
+            </View>
+          </View>
+        </View>
+      );
+    }
+    
+    export default DetailProductScreen;   
+
+
+
+
+
+
+
+
+
+/*
 function DetailProductScreen() {
 
     function Test(){
@@ -22,4 +72,4 @@ function DetailProductScreen() {
     );
 }
 
-export default DetailProductScreen;
+export default DetailProductScreen; */
