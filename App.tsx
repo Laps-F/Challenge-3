87a871d6fafe8d43b9ba9fd6891f0572d97d9376
Laps-F <maurolaps@gmail.com>
2023-05-27 01:43:16 -0300

    feat: Using the useContext hook on the App

--- App.tsx
+++ App.tsx
@@ -11,64 +11,67 @@ import ShoppingCartScreen from './screens/CartScreen';
 import { Colors, NewColors } from './constants/styles';
 
 import { UnauthenticatedStackParams } from './types/Navigation';
+import FavoritesContextProvider from './store/context/FavoriteProducts';
 
 const Stack = createStackNavigator<UnauthenticatedStackParams>();
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen 
-          name="WelcomeScreen"
-          component={WelcomeScreen} 
-          options={{
-            headerShown: false
-          }}
-        />
-        <Stack.Screen 
-          name="SignUpScreen" 
-          component={SignUpScreen} 
-          options={{
-            headerStyle: {backgroundColor: Colors.background},
-            title: '',
-            headerTintColor: Colors.primary,
-            headerShadowVisible: false,
-          }}
-        />
-        <Stack.Screen 
-          name="HomeScreen" 
-          component={HomeScreen} 
-          options={{
-            headerLeft: ()=> null,
-            headerStyle: {backgroundColor: Colors.background},
-            title: 'HOME',
-            headerTintColor: Colors.primary,
-            headerShadowVisible: false,
-          }}
-        />
-        <Stack.Screen 
-          name="DetailProductScreen"
-          component={DetailProductScreen}
-          options={{
-            headerStyle: {backgroundColor: NewColors.background, height: 50},
-            title: '',
-            headerTintColor: Colors.primary,
-            headerShadowVisible: false,
-          }}
-        />
-        <Stack.Screen 
-          name="ShoppingCartScreen"
-          component={ShoppingCartScreen}
-          options={{
-            headerLeft: ()=> null,
-            headerStyle: {backgroundColor: NewColors.background},
-            title: '',
-            headerTintColor: Colors.primary,
-            headerShadowVisible: false,
-          }}
-        />
-      </Stack.Navigator>
-      <StatusBar style="light" />
-    </NavigationContainer>
+    <FavoritesContextProvider>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen 
+            name="WelcomeScreen"
+            component={WelcomeScreen} 
+            options={{
+              headerShown: false
+            }}
+          />
+          <Stack.Screen 
+            name="SignUpScreen" 
+            component={SignUpScreen} 
+            options={{
+              headerStyle: {backgroundColor: Colors.background},
+              title: '',
+              headerTintColor: Colors.primary,
+              headerShadowVisible: false,
+            }}
+          />
+          <Stack.Screen 
+            name="HomeScreen" 
+            component={HomeScreen} 
+            options={{
+              headerLeft: ()=> null,
+              headerStyle: {backgroundColor: Colors.background},
+              title: 'HOME',
+              headerTintColor: Colors.primary,
+              headerShadowVisible: false,
+            }}
+          />
+          <Stack.Screen 
+            name="DetailProductScreen"
+            component={DetailProductScreen}
+            options={{
+              headerStyle: {backgroundColor: NewColors.background, height: 50},
+              title: '',
+              headerTintColor: Colors.primary,
+              headerShadowVisible: false,
+            }}
+          />
+          <Stack.Screen 
+            name="ShoppingCartScreen"
+            component={ShoppingCartScreen}
+            options={{
+              headerLeft: ()=> null,
+              headerStyle: {backgroundColor: NewColors.background},
+              title: '',
+              headerTintColor: Colors.primary,
+              headerShadowVisible: false,
+            }}
+          />
+        </Stack.Navigator>
+        <StatusBar style="light" />
+      </NavigationContainer>
+    </FavoritesContextProvider>
   );
 }
