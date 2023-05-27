3d16512c600b688047b98341eb28ca94e5fb9363
Laps-F <maurolaps@gmail.com>
2023-05-27 20:36:13 -0300

    refactor: Change the use os the redux Provider

--- App.tsx
+++ App.tsx
@@ -17,69 +17,65 @@ import FavoritesContextProvider from './store/context/FavoriteProducts';
 
 const Stack = createStackNavigator<UnauthenticatedStackParams>();
 
-const ShoppingCartScreenWrapper = () => (
-  <Provider store={store}>
-    <ShoppingCartScreen />
-  </Provider>
-);
-
 export default function App() {
   return (
     <FavoritesContextProvider>
-      <NavigationContainer>
-        <Stack.Navigator>
-          <Stack.Screen 
-            name="WelcomeScreen"
-            component={WelcomeScreen} 
-            options={{
-              headerShown: false
-            }}
-          />
-          <Stack.Screen 
-            name="SignUpScreen" 
-            component={SignUpScreen} 
-            options={{
-              headerStyle: {backgroundColor: Colors.background},
-              title: '',
-              headerTintColor: Colors.primary,
-              headerShadowVisible: false,
-            }}
-          />
-          <Stack.Screen 
-            name="HomeScreen" 
-            component={HomeScreen} 
-            options={{
-              headerLeft: ()=> null,
-              headerStyle: {backgroundColor: Colors.background},
-              title: 'HOME',
-              headerTintColor: Colors.primary,
-              headerShadowVisible: false,
-            }}
-          />
-          <Stack.Screen 
-            name="DetailProductScreen"
-            component={DetailProductScreen}
-            options={{
-              headerStyle: {backgroundColor: NewColors.background, height: 50},
-              title: '',
-              headerTintColor: Colors.primary,
-              headerShadowVisible: false,
-            }}
-          />
-          <Stack.Screen 
-            name="ShoppingCartScreen"
-            component={ShoppingCartScreenWrapper}
-            options={{
-              headerLeft: ()=> null,
-              headerStyle: {backgroundColor: NewColors.background},
-              title: '',
-              headerTintColor: Colors.primary,
-              headerShadowVisible: false,
-            }}
-          />
-        </Stack.Navigator>
-        <StatusBar style="light" />
-      </NavigationContainer>
+      <Provider store={store}>
+        <NavigationContainer>
+          <Stack.Navigator>
+            <Stack.Screen 
+              name="WelcomeScreen"
+              component={WelcomeScreen} 
+              options={{
+                headerShown: false
+              }}
+            />
+            <Stack.Screen 
+              name="SignUpScreen" 
+              component={SignUpScreen} 
+              options={{
+                headerStyle: {backgroundColor: Colors.background},
+                title: '',
+                headerTintColor: Colors.primary,
+                headerShadowVisible: false,
+              }}
+            />
+            <Stack.Screen 
+              name="HomeScreen" 
+              component={HomeScreen} 
+              options={{
+                headerLeft: ()=> null,
+                headerStyle: {backgroundColor: Colors.background},
+                title: 'HOME',
+                headerTintColor: Colors.primary,
+                headerShadowVisible: false,
+              }}
+            />
+            <Stack.Screen 
+              name="DetailProductScreen"
+              component={DetailProductScreen}
+              options={{
+                headerStyle: {backgroundColor: NewColors.background, height: 50},
+                title: '',
+                headerTintColor: Colors.primary,
+                headerShadowVisible: false,
+              }}
+            />
+            <Stack.Screen 
+              name="ShoppingCartScreen"
+              component={ShoppingCartScreen}
+              options={{
+                headerLeft: ()=> null,
+                headerStyle: {backgroundColor: NewColors.background},
+                title: '',
+                headerTintColor: Colors.primary,
+                headerShadowVisible: false,
+              }}
+            />
+          </Stack.Navigator>
+          <StatusBar style="light" />
+        </NavigationContainer>
+      </Provider>
     </FavoritesContextProvider>
   );
 }
