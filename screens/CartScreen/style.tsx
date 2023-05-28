ca0ddd88cede06c59da7b391e7ccb4b63a309c6d
Arthur Alexandre Cunha Custodio <arthur.custodio@aluno.ufop.edu.br>
2023-05-28 15:51:51 -0300

    making the firsts modifications on screen style

--- screens/CartScreen/style.tsx
+++ screens/CartScreen/style.tsx
@@ -5,36 +5,72 @@ const HEIGHT = Dimensions.get('window').height;
 const WIDTH = Dimensions.get('window').width;
 
 export const styles = StyleSheet.create({
-    root: {
-        flex: 1,
-        backgroundColor: NewColors.background,
-    },
-    totalPriceContainer: {
-        height: 60,
-        alignItems: "flex-start",
-        left: "10%",
-        top: "20%",
-        //backgroundColor: NewColors.primary,
-    },
-    totalText: {
-        color: "white",
-        fontSize: 20,
-    },
-    totalValue: {
-        color: NewColors.primary,
-        fontSize: 32,
-    },
-    buttonContainer: {
-        top: HEIGHT - 200,
-        width: "100%",
-        position: 'absolute',
-        justifyContent: 'center',
-        alignItems: 'center',
-    },
-    button: {
-        flex: 1,
-        width: "50%",
-        borderRadius: 10,
-        backgroundColor: NewColors.buttonBuy_Add,
-    },
+  root: {
+    flex: 1,
+    backgroundColor: NewColors.background,
+  },
+  totalPriceContainer: {
+    display: "flex",
+    left: "10%",
+  },
+  totalText: {
+    color: "white",
+    fontSize: 20,
+  },
+  totalValue: {
+    color: NewColors.primary,
+    fontSize: 32,
+  },
+  productsContainer: {
+    top: 10,
+    flexDirection: "row",
+    justifyContent: 'center',
+    //backgroundColor: NewColors.primary,
+  },
+  cardContainer: {
+    position: "relative",
+    flexDirection: "row",
+    alignItems: "center",
+    width: "85%",
+    height: 129,
+    backgroundColor: "#eee",
+    borderRadius: 5,
+  },
+  cardImage: {
+    width: 122,
+    height: 122,
+    marginRight: 10,
+  },
+  cardTitle: {
+    fontSize: 14,
+    fontWeight: "bold",
+    marginBottom: 5,
+  },
+  cardPrice: {
+    fontSize: 16,
+    color: NewColors.primary,
+    fontWeight: '600',
+    //backgroundColor: NewColors.primary,
+  },
+
+  cardInfoContainer: {
+    flex: 1,
+  },
+  buttonContainer: {
+    width: "100%",
+    //marginTop: 20,
+    justifyContent: 'center',
+    alignItems: 'center',
+    
+  },
+  button: {
+    width: "50%",
+    borderRadius: 10,
+    backgroundColor: NewColors.buttonBuy_Add,
+  },
+  listContainer: {
+    height: HEIGHT * 0.65,
+  },
+
+
 });
