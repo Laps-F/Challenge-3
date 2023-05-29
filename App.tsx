a70f6146088a41fb22e0f65b541af313c817c3d0
Arthur Alexandre Cunha Custodio <arthur.custodio@aluno.ufop.edu.br>
2023-05-29 00:15:19 -0300

    Styling tab bar

--- App.tsx
+++ App.tsx
@@ -1,6 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+import Ionicons from '@expo/vector-icons/Ionicons';
 import { Provider } from 'react-redux';
 import { store } from './redux/store';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
@@ -66,14 +67,31 @@ function MainTabNavigator() {
   return (
     <Tab.Navigator
       screenOptions={
-        { headerShown: false, }
-      }
+        {
+          headerShown: false,
+          tabBarStyle: {
+            backgroundColor: '#2D2D2D',
+          },
+          tabBarActiveTintColor: "#D78F3C",
+        }}
     >
       <Tab.Screen name="HomeScreen"
         component={HomeScreen}
+        options={{
+          tabBarLabel: 'Home',
+          tabBarIcon: ({ color, size, focused }) => (
+            <Ionicons name="home" size={size} color={focused ? NewColors.primary : "#FFFFFF"} />
+          ),
+        }}
       />
       <Tab.Screen name="ShoppingCartScreen"
         component={ShoppingCartScreen}
+        options={{
+          tabBarLabel: 'Cart',
+          tabBarIcon: ({ color, size, focused }) => (
+            <Ionicons name="cart" size={size} color={focused ? NewColors.primary : "#FFFFFF"} />
+          ),
+        }}
       />
     </Tab.Navigator>
   );
